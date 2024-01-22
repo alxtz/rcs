3336a7e0cde9934f1274b68294378039c417f6fe
alxtz <alex.tzeng@xendit.co>
2024-01-22 19:57:33 +0800

    cmt

--- go/cmd/0/main.go
+++ go/cmd/0/main.go
@@ -7,3 +7,59 @@ import (
 func main() {
 	fmt.Println("Hello, World")
 }
+
+type Cache struct {
+	Map         map[string]string
+	MaxCapacity int
+}
+
+// skip the constructor
+
+func (c *Cache) Get(key string) *string {
+	val, ok := c.Map[key]
+
+	fmt.Println(c.Map)
+
+	if !ok {
+		return nil
+	}
+
+	return &val
+}
+
+func (c *Cache) Set(key string, val string) {
+	_, alreadyExists := c.Map[key]
+
+	if alreadyExists {
+		c.Map[key] = val
+		return
+	}
+
+	currCap := c.CurrCapacity()
+	maxCap := c.MaxCapacity
+
+	// fmt.Println("curr", currCap, "max", maxCap)
+
+	if currCap == maxCap {
+		keys := []string{}
+
+		for existingKey := range c.Map {
+			keys = append(keys, existingKey)
+		}
+
+		keyToRm := keys[0]
+
+		// delete a key
+		delete(c.Map, keyToRm)
+
+	}
+
+	// add in a new key
+	c.Map[key] = val
+
+	// implement kicking
+}
+
+func (c *Cache) CurrCapacity() int {
+	return len(c.Map)
+}
--- go/cmd/0/main_test.go
+++ go/cmd/0/main_test.go
@@ -6,11 +6,108 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ShouldDoA_WhenGivenB(t *testing.T) {
-	// t.Skip()
+func Test_ShouldGetValWorkAfterSetting(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 1}
+
+	c.Set("Oranges", "juicy")
+
 	assert.Exactly(
 		t,
-		[]int{1, 2, 3},
-		[]int{1, 2, 3},
+		"juicy",
+		*(c.Get("Oranges")),
+	)
+}
+
+func Test_ShouldWorkAfterSetting2Times(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 1}
+	c.Set("Oranges", "juicy")
+	c.Set("Oranges", "not-juicy")
+
+	assert.Exactly(
+		t,
+		"not-juicy",
+		*(c.Get("Oranges")),
+	)
+}
+
+func Test_ShouldNilWhenGettingNonExistKey(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 1}
+	c.Set("Apple", "nah")
+
+	assert.Nil(
+		t,
+		c.Get("Oranges"),
+	)
+}
+
+// should the max capacity work
+func Test_ShouldHaveAFunctionalMaxCapacity(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 10}
+	c.Set("Apple", "nah")
+
+	assert.Exactly(
+		t,
+		10,
+		c.MaxCapacity,
+	)
+}
+
+// should the current map capacity itself by respected
+func Test_ShouldHaveAFunctionalMapCapacity(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 10}
+	c.Set("Apple", "nah")
+	c.Set("Apple2", "nah")
+
+	assert.Exactly(
+		t,
+		2,
+		c.CurrCapacity(),
+	)
+}
+
+// there should not be a chance for the CurrCap to exceed MaxCap
+func Test_ShouldNotExceedMaxCap(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 3}
+	c.Set("Apple", "nah")
+	c.Set("Apple2", "nah")
+	c.Set("Apple3", "nah")
+	c.Set("Apple4", "nah")
+
+	assert.Exactly(
+		t,
+		3,
+		c.CurrCapacity(),
+	)
+}
+
+// when MaxCap is exceeded, kickout the leasted used item
+func Test_ShouldKickLeastUsed_WhenExceedsCap(t *testing.T) {
+	var c = Cache{Map: make(map[string]string), MaxCapacity: 3}
+	c.Set("Apple", "nah0")
+	c.Set("Apple2", "nah1")
+	c.Set("Apple3", "nah2")
+
+	c.Get("Apple2")
+	c.Get("Apple3")
+
+	// apple should exist
+	assert.Exactly(
+		t,
+		"nah0",
+		c.Get("Apple"),
+	)
+
+	c.Set("Apple4", "nah3") // should remove apple
+
+	// apple4 should exist
+	assert.Exactly(
+		t,
+		"nah3",
+		c.Get("Apple4"),
+	)
+	// apple should be missing
+	assert.Nil(
+		t,
+		c.Get("Apple"),
 	)
 }
